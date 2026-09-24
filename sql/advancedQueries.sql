f6fb84206a12ddc089f6d3dbd1d72409e59e71be
surprise2024-cpu <surprise2024@gmail.com>
2026-09-24 21:40:56 +0200

    add SQL queries for advanced book and author searches

---
+++ sql/advancedQueries.sql
@@ -0,0 +1,29 @@
+-- Find books published after 1950
+SELECT *
+FROM books
+WHERE published_year > 1950;
+
+-- Find all American authors
+SELECT *
+FROM authors
+WHERE nationality = 'American';
+
+-- Set all books as available
+UPDATE books
+SET available = TRUE;
+
+-- Find all available books after 1950
+SELECT *
+FROM books
+WHERE available = TRUE
+AND published_year > 1950;
+
+-- Find authors whose names contain "George"
+SELECT *
+FROM authors
+WHERE name ILIKE '%George%';
+
+-- Incremenet published year 1869 by 1
+UPDATE books 
+SET published_year = published_year + 1
+WHERE published_year = 1869;
