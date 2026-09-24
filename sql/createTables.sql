0743e4607a26cc6e3fe73551c0332eafe8f7aa99
surprise2024-cpu <surprise2024@gmail.com>
2026-09-24 19:14:24 +0200

    add initial SQL scripts for authors, books, and patrons tables

---
+++ sql/createTables.sql
@@ -0,0 +1,32 @@
+/**/
+CREATE TABLE authors (
+
+    id INT PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    nationality VARCHAR(100),
+    birth_year INT,
+    death_year INT
+);
+
+CREATE TABLE books (
+
+    id INT PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    author_id INT NOT NULL,
+    genres TEXT[],
+    published_year INT,
+    available BOOLEAN DEFAULT TRUE,
+
+    CONSTRAINT fk_author
+        FOREIGN KEY (author_id)
+        REFERENCES authors(id)
+        ON DELETE RESTRICT 
+);
+
+CREATE TABLE patrons (
+
+    id INT PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    email VARCHAR(200) UNIQUE NOY NULL,
+    borrowed_books INT[] DEFAULT '{}'
+);
