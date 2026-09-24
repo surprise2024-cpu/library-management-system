cddefa874fab9b6a3f23d5cd32cd778064dfde71
surprise2024-cpu <surprise2024@gmail.com>
2026-09-24 21:13:58 +0200

    add SQL update queries for borrowing books and managing genres

---
+++ sql/updateQueries.sql
@@ -0,0 +1,14 @@
+-- Mark a book as borrowed
+UPDATE books
+SET available = FALSE
+WHERE id = 1;
+
+-- Add a new genre to a hook
+UPDATE books
+SET genres = array_append(genres, 'Classic')
+WHERE id = 1;
+
+-- Add a borrowed book to a patron
+UPDATE patrons
+SET borrowed_books = array_append(borrowed_books, 3)
+WHERE id = 1;
